70de314c178d8789d6dfcd1d98debca003cd7c30
kazu4sand <56918411+kazu4sand@users.noreply.github.com>
2020-10-30 10:58:46 +0900

    Update OverflowCheck.c

--- OverflowCheck.c
+++ OverflowCheck.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 int main(void){
-    // Your code here!
-    unsigned int incnum = 0xFFFFFFFE;
-    unsigned int tmp    = ~(incnum);
-    unsigned int addrL  = 0x00000050;
-    unsigned int addrH  = 0x0;
+    /* 32bit環境しか使えないけど64bitを使いたい */
+    /* さらに足し算時のオーバーフローを考慮したい場合 */
+    unsigned int incnum = 0xFFFFFFFE;   /* 加算する数               */
+    unsigned int tmp    = ~(incnum);    /* 加算する数を反転させた値 */
+    unsigned int numL  = 0x00000050;    /* 加算される数(下位32bit)  */
+    unsigned int numH  = 0x0;           /* 加算される数(上位32bit)  */
     
-    if(tmp < addrL)
+    if(tmp < numL)
     {
         puts("桁あふれ");
-        addrH++;
-        addrL = addrL - (tmp + 1);
-        printf("0x%x_%x",addrH,addrL);
+        numH++;
+        numL = numL - (tmp + 1);
+        printf("0x%x_%x",numH,numL);
     }
     else
     {
         puts("桁あふれしない");
-        addrL = addrL + incnum;
-        printf("0x%x_%x",addrH,addrL);
+        numL = numL + incnum;
+        printf("0x%x_%x",numH,numL);
     }
 }
